f25907bf954a3f56a1ac6621519661c04c1498c4
Athena <jud_mais@hotmail.com>
2017-06-10 17:35:19 -0300

    Atualizando interface.

--- src/interfaceDoPrograma/Interface.java
+++ src/interfaceDoPrograma/Interface.java
@@ -4,9 +4,13 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+import javax.swing.JTextPane;
 
 public class Interface extends JFrame implements ActionListener{
 	
@@ -24,15 +28,29 @@ public class Interface extends JFrame implements ActionListener{
 	JMenuItem mItem3;
 	JMenuItem mItem4;
 	
+	JTextArea painelPrincipal;
+	
 	public Interface(){
 		setTitle("Projeto LP2");
-	    setSize(500,500);
-	     
+	    setSize(300,300);
+	    this.setLayout(null);
 	    // Cria uma barra de menu para o JFrame
 	    JMenuBar menuBar = new JMenuBar();
-	        
 	    // Adiciona a barra de menu ao  frame
 	    setJMenuBar(menuBar);
+	    
+	    painelPrincipal = new JTextArea();
+	    painelPrincipal.setEditable(false);
+	    painelPrincipal.setLineWrap(true);
+	    painelPrincipal.setSize(290, 290);
+	    painelPrincipal.setBounds(10, 10, 260, 220);
+	    this.add(painelPrincipal);
+	    painelPrincipal.setText("Dados registrados no sistema \n" + 
+	    "Usuarios: \n" + 
+	    "Devices: " + "\n" +
+	    "Http: " +  "\n" +
+	    "Logon: " + "\n");
+	    
 	    
 	    logMenu = new JMenu("Cadastrar Logs");
 		relaMenu = new JMenu("Relatorios");
@@ -48,14 +66,17 @@ public class Interface extends JFrame implements ActionListener{
         mItem1 = new JMenuItem("Logs", 69);
         mItem2 = new JMenuItem("Arvore", 70);
         mItem3 = new JMenuItem("Geral");
+        mItem4 = new JMenuItem("Salvar e sair");
         
         logMenu.add(mItem1);
         arvMenu.add(mItem2);
         relaMenu.add(mItem3);
+        sairMenu.add(mItem4);
         
         mItem1.addActionListener(this);
         mItem2.addActionListener(this);
         mItem3.addActionListener(this);
+        mItem4.addActionListener(this);
 	}
 
 	
@@ -71,10 +92,14 @@ public class Interface extends JFrame implements ActionListener{
 			addLog.setVisible(true);
 		}
 		if (evento.getSource() == mItem2){
-			//tela.setTitle("Fornecedores");
+			
 		}
 		if (evento.getSource() == mItem3){
-			//tela.setTitle("Produtos");
+			
+		}
+		if (evento.getSource() == mItem4){
+			//chamar função gravarCSV;
+			System.exit(0);
 		}
 	}
 }
